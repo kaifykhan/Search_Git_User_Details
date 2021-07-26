27db1e3ea406c31adcafbfb453b20bc24f456dc7
Kaifi_Khan <kaifykhan988@gmail.com>
2021-07-26 12:00:43 +0530

    Added few more fields in card components

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Card = ({ getdata }) => { 
   return (
@@ -17,8 +18,25 @@ const Card = ({ getdata }) => {
               </div>
               <div className="col-md-8">
                 <div className="card-body">
-                  <h5 className="card-title">{getdata && getdata.login}</h5>
+                  <h5 className="card-title">{getdata && getdata.name}</h5>
+                  <p className="card-text">{getdata && getdata.company}</p>
+                </div>
+              </div>
+            </div>
+            <div className="row g-0">
+              <div className="col-12">
+                <div className="card-body">
+                  <h5 className="card-title">{getdata && getdata.bio}</h5>
                   <p className="card-text">{getdata && getdata.location}</p>
+                </div>
+              </div>
+              <div className="col-12">
+                <div className="card-body">
+                  <h5 className="card-title text-start">
+                    <button className="btn btn-outline-dark" type="button">
+                      <Link to={getdata && getdata.url} className="card_a">GO TO GITHUB</Link>
+                    </button>
+                  </h5>
                 </div>
               </div>
             </div>
